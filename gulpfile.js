90f260c43305000ee1050f69d4df2fb317b8b482
satoshiyoshimura <satoshi.yoshimura.so@gmail.com>
2014-11-27 02:20:22 +0900

    firstcommit でほぼ完成

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var watch = require('gulp-watch');
+var stylus = require('gulp-stylus');
+var fs = require('fs');
+var gulpmatch = require('gulp-match');
+var map = require('map-stream');
+
+gulp.task('watch', function () {
+  gulp.watch('./jade/*.jade', ['jade']);
+  gulp.watch('./stylus/*.styl', ['stylus']);
+});
+
+gulp.task('jade', function () {
+  // 最新のJSONファイルを同期読み込みしてオブジェクトを生成
+  var json = JSON.parse(fs.readFileSync("./i18n/ja.json"));
+  gulp.src('./jade/*.jade')  // gulp.src でファイルを指定
+  .pipe(jade(json))                  // jade plugin に通して
+  .pipe(gulp.dest('./'))  // gulp.dest で書きだす
+});
+
+gulp.task('stylus', function() {
+  gulp.src('stylus/*')
+  .pipe(stylus())
+  .pipe(gulp.dest('css/'));
+});
